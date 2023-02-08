282dacd6ff135f11c10f7d3176ecea923dca7e7a
Glenn Eggleton <geggleto@gmail.com>
2023-02-08 10:03:57 -0500

    Add logging

--- public/index.php
+++ public/index.php
@@ -1,21 +1,26 @@
 <?php
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$log = new Logger('php');
+$log->pushHandler(new StreamHandler(__DIR__ . '/../logs/php-app.log', Level::Info));
+
 $app = AppFactory::create();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
 
-$app->get('/', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write('Hello!');
-
+$app->get('/', function (Request $request, Response $response, array $args) use ($log) {
+    $response->getBody()->write(\json_encode($request->getServerParams()));
+    $log->info('REQUEST HANDLED ON', [
+        $request->getServerParams()['SERVER_ADDR'],
+        $request->getServerParams()['HOSTNAME']
+    ]);
     return $response;
 });
 
